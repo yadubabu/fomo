7b1d784ce900ff11ca5ab3b7e06c227cf1f9636c
yadubabu <100501154+yadubabu@users.noreply.github.com>
2024-07-19 23:14:51 +0530

    Add files via upload

---
+++ assets/pics.tsx
@@ -0,0 +1,10 @@
+export const pics=[
+    require('./eth.webap'),
+    require('./btc.webap'),
+    require('./usdt.webap'),
+    require('./bnb.webap'),
+    require('./sol.webap'),
+
+
+    // btc:require('../assets/btc.webp')
+]
