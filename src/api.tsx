e538ba3e6885e348883754d0e853a556bfdb7625
yadubabu <100501154+yadubabu@users.noreply.github.com>
2024-07-19 23:31:31 +0530

    Add files via upload

---
+++ src/api.tsx
@@ -0,0 +1,5 @@
+export const getCoinsList='http://localhost:5000/coins/list'
+
+export const setCoinsList='http://localhost:5000/coins/add';
+
+export const getStockapi='http://localhost:5000/exchanges/list';
