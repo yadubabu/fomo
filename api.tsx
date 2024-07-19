19251e378c4d13a8ebeaa5b6647f49b9d91c046c
yadubabu <100501154+yadubabu@users.noreply.github.com>
2024-07-19 23:32:52 +0530

    Delete api.tsx

--- api.tsx
+++
@@ -1,5 +0,0 @@
-export const getCoinsList='http://localhost:5000/coins/list'
-
-export const setCoinsList='http://localhost:5000/coins/add';
-
-export const getStockapi='http://localhost:5000/exchanges/list';
