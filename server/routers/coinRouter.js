81fd6ac6b54dbe6d64aeba83156213aecd7ccded
yadubabu <100501154+yadubabu@users.noreply.github.com>
2024-07-20 11:34:16 +0530

    Update coinRouter.js

--- server/routers/coinRouter.js
+++ server/routers/coinRouter.js
@@ -1,8 +1,10 @@
 const express=require('express');
-const coinSchema=require('../models/coinSchema');
-const getCoins=require('../controllers/coinController')
+ const getCoins=require('../controllers/coinsListController')
 const coinRouter=express.Router();
+const getSingleCoin=require('../controllers/coinController')
 
 coinRouter.route('/list').get(getCoins);
+coinRouter.route('/single/:code').get(getSingleCoin);
+
 
 module.exports=coinRouter;
