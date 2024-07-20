08b4f6c3296f6bd6f20f522ee07ec53efd18175c
yadubabu <100501154+yadubabu@users.noreply.github.com>
2024-07-20 11:33:15 +0530

    Update coinController.js

--- server/controllers/coinController.js
+++ server/controllers/coinController.js
@@ -1,4 +1,3 @@
-
 const coinSchema = require("../models/coinSchema");
 const { MongoClient } = require('mongodb');
 
@@ -9,61 +8,119 @@ const { MongoClient } = require('mongodb');
 // }
 
 
-const getCoins=async(req,res)=>{
-    await fetch(new Request(`${url}/coins/list`), {
-        method: "POST",
-        headers: new Headers({
-          "content-type": "application/json",
-          "x-api-key": apiKey,
-        }),
-        body: JSON.stringify({
-          currency: "USD",
-          sort: "rank",
-          order: "ascending",
-          offset: 0,
-          limit: 20,
-          meta: false,
-        }),
-      }).then((res)=>res.json()).then(async(data)=>{
-        // await coinSchema.find()
-       res.send(data)
-        try{
+// export const getCoins=async(req,res)=>{
+//     await fetch(new Request(`${url}/coins/list`), {
+//         method: "POST",
+//         headers: new Headers({
+//           "content-type": "application/json",
+//           "x-api-key": apiKey,
+//         }),
+//         body: JSON.stringify({
+//           currency: "USD",
+//           sort: "rank",
+//           order: "ascending",
+//           offset: 0,
+//           limit: 20,
+//           meta: false,
+//         }),
+//       }).then((res)=>res.json()).then(async(data)=>{
+//         // await coinSchema.find()
+//        res.send(data)
+//         try{
 
-const uri = "mongodb+srv://mohini:mohinimohini@cluster0.sqdke.mongodb.net/";
+// const uri = "mongodb+srv://mohini:mohinimohini@cluster0.sqdke.mongodb.net/";
 
-const client = new MongoClient(uri);
+// const client = new MongoClient(uri);
 
-async function run() {
-  try {
-    await client.connect();
+// async function run() {
+//   try {
+//     await client.connect();
     
-    // Replace 'databaseName' and 'collectionName' with your database and collection names
-    const database = client.db('fomo-factory');
-    const collection = database.collection('coinschemas');
+//     // Replace 'databaseName' and 'collectionName' with your database and collection names
+//     const database = client.db('fomo-factory');
+//     const collection = database.collection('coinschemas');
 
-    const deleteResult = await collection.deleteMany({});
-    console.log(`${deleteResult.deletedCount} documents were deleted`);
+//     const deleteResult = await collection.deleteMany({});
+//     console.log(`${deleteResult.deletedCount} documents were deleted`);
     
-    // Insert the new array of data
-    const insertResult = await collection.insertMany(data);
-    console.log(`${insertResult.insertedCount} documents were inserted`);
+//     // Insert the new array of data
+//     const insertResult = await collection.insertMany(data);
+//     console.log(`${insertResult.insertedCount} documents were inserted`);
 
-  } 
-  finally {
+//   } 
+//   finally {
     
-        await client.close();
+//         await client.close();
 
-  }
-}
+//   }
+// }
 
-run().catch(console.dir);
+// run().catch(console.dir);
 
      
-        }
-        catch(err){
-          console.log(err);
-        }
+//         }
+//         catch(err){
+//           console.log(err);
+//         }
          
-      })
+//       })
+// }
+// module.exports=getCoins;
+
+ const getSingleCoin=async(req,res)=>{
+  const code = req.params.code.toUpperCase();
+  console.log(code);
+  await fetch(new Request("https://api.livecoinwatch.com/coins/single"), {
+    method: "POST",
+    headers: new Headers({
+      "content-type": "application/json",
+      "x-api-key": apiKey,
+    }),
+    body: JSON.stringify({
+      currency: "USD",
+      code: code,
+      meta: true,
+    }),
+  }).then((res)=>res.json()).then(async(data)=>{
+      // await coinSchema.find()
+     res.send(data)
+//       try{
+
+// const uri = "mongodb+srv://mohini:mohinimohini@cluster0.sqdke.mongodb.net/";
+
+// const client = new MongoClient(uri);
+
+// async function run() {
+// try {
+//   await client.connect();
+  
+//   // Replace 'databaseName' and 'collectionName' with your database and collection names
+//   const database = client.db('fomo-factory');
+//   const collection = database.collection('coinschemas');
+
+//   const deleteResult = await collection.deleteMany({});
+//   console.log(`${deleteResult.deletedCount} documents were deleted`);
+  
+//   // Insert the new array of data
+//   const insertResult = await collection.insertMany(data);
+//   console.log(`${insertResult.insertedCount} documents were inserted`);
+
+// } 
+// finally {
+  
+//       await client.close();
+
+// }
+// }
+
+// run().catch(console.dir);
+
+   
+//       }
+//       catch(err){
+//         console.log(err);
+//       }
+       
+    })
 }
-module.exports=getCoins;
+module.exports=getSingleCoin;
