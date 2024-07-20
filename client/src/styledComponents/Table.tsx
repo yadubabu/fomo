f124becd1ee10fcaa08d5f8598246a437c93b842
yadubabu <100501154+yadubabu@users.noreply.github.com>
2024-07-20 22:45:17 +0530

    Update Table.tsx

--- client/src/styledComponents/Table.tsx
+++ client/src/styledComponents/Table.tsx
@@ -1,18 +1,29 @@
-
 import React,{useState} from 'react';
 import { useSelector } from 'react-redux';
 import { AppState } from '../redux/store';
-import { Coins } from '../dataTypes';
+import { Coin, Coins } from '../dataTypes';
 import { pics } from '../pics';
 import { Link } from 'react-router-dom';
 import './style.css'
+import TableModel from './TableModel';
 
 type Props = {}
 
 const TableComp = (props: Props) => {
-  const [showModal,setShowModal]=useState('none')
-
+  const [showModal,setShowModal]=useState(false)
+  const [data,setData]=useState<Coin | any>();
+  const [key,setKey]=useState('');
 const coins=useSelector<AppState,Coins[]>(state=>state.coins)
+const showCard=(id:string)=>{
+    fetch(`http://localhost:5000/coins/single/${id}`)
+    .then(res=>res.json())
+    .then(data=>setData(data))
+    .then(()=>setShowModal(!showModal))
+    .then(()=>setKey(id))
+    .catch(err=>console.log(err)
+    )
+}
+
   return (
     <div className='bg-slate-800 rounded-md m-5 shadow-2xl text-center  pt-5'>
         <div className='flex justify-start'>
@@ -30,16 +41,13 @@ const coins=useSelector<AppState,Coins[]>(state=>state.coins)
             <div className="table-cell text-center ...">Volume</div>
           </div>
         </div>
-        <div className={`${showModal}`}>
-          <div className='text-end p-1 m-4'>
-            <button className='text-black font-bold text-2xl border-red-100' onClick={()=>setShowModal('none')}>X</button>
-          </div>
-        </div>
+        {/* <div className={showModal}></div> */}
 
         {coins[0].cap !== null && coins.slice(0,20).map((coin:Coins,index:number=0)=>{
           return(<>
+         {showModal ? (<><TableModel data={data} code={key} setShowModel={setShowModal}/></>):''}
           <hr/>
-            <div onClick={()=>setShowModal('display')} key={index} className="tbody table-row ... p-5 m-5 bg-gray-800">
+            <div id={coin.code} onClick={() => showCard(`${coin.code}`)} key={index} className="tbody table-row ... p-5 m-5 bg-gray-800">
               <div className="table-cell ... p-7">{index+1}</div>
               <div className="table-cell ... code">{coin.code}</div>
               <div className="table-cell ..."><img className='w-1/5 -my-4 ' src={require(`../assets/${coin.code.toLowerCase()}.webp`)} alt=''/></div>
