f3da0956081c3b61b2c3ed3c9449176e3f6c6402
yadubabu <100501154+yadubabu@users.noreply.github.com>
2024-07-20 22:42:30 +0530

    Update stockReducer.tsx

--- client/src/redux/reducers/stockReducer.tsx
+++ client/src/redux/reducers/stockReducer.tsx
@@ -5,15 +5,15 @@ import { Coins, Stocks } from '../../dataTypes'
 const initialValue:Stocks[]=[
     {
         name: '4swap',
-        volume:0,
-        markets:0,
-        askTotal:0,
-        bidTotal:0,
+        volume:1,
+        markets:1,
+        askTotal:1,
+        bidTotal:1,
         code:'gemini',
         png64: 'https://lcw.nyc3.cdn.digitaloceanspaces.com/production/exchanges/64/4swap.png',
         png128: 'https://lcw.nyc3.cdn.digitaloceanspaces.com/production/exchanges/128/4swap.png',
         webp64: 'https://lcw.nyc3.cdn.digitaloceanspaces.com/production/exchanges/64/4swap.webp',
-        webp128: 'https://lcw.nyc3.cdn.digitaloceanspaces.com/production/exchanges/64/4swap.webp'
+        webp128: 'https://lcw.nyc3.cdn.digitaloceanspaces.com/production/exchanges/128/4swap.webp'
     },
 ]
     
@@ -26,9 +26,6 @@ export interface StockState{
 
 // eslint-disable-next-line no-empty-pattern
 const stockReduser=(state=initialValue,action:StockState)=> {
-    console.log(action.payload);
-    
-    
   if(action.type===Types.GET_STOCKS){
     return [...action.payload]
     
